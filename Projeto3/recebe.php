fb08e55acc4e5c59e35cb2ba6faa5b89ee81e250
Mauri <39652613+PCNmauri@users.noreply.github.com>
2020-08-01 22:10:46 -0300

    uploadtabala

--- Projeto3/recebe.php
+++ Projeto3/recebe.php
@@ -1,67 +1,63 @@
 <?php
-    //$erro = isset($_GET['erro']) ? $_GET['erro'] : null
+
     if(($_POST['nomearq'] != "") && ($_FILES['arquivo'] ['name'] != "")){
         $nomearq = isset($_POST['nomearq']) ? $_POST['nomearq'] : null;
 
-    //echo'Este script PHP recebe um arquivo e salva o arquivo na pasta \'enviados\' dentro deste site <br> <br>';
-    
-    //echo'Conteudo da variavel nativa $_FILES:<br><br>';
-    
-    //echo 'name (nome original do arquivo na maquina do usuario): '.$_FILES['arquivo'] ['name'].'<br>';
-    
-    //pasta dentro do site onde serão salvos os arquivos enviados
-    $dir = 'enviados';
-    // Verifica se houve algum erro com o upload. Se sim, exibe a mensagem do erro
-    if ($_FILES['arquivo'] ['error'] != 0)  {
-        die ("Não foi possivel fazer o upload <br>");
-        exit; //para a execução do script
+        
+        
+        if ($_FILES['arquivo'] ['error'] != 0)  {
+            die ("Não foi possivel fazer o upload <br>");
+            exit; //para a execução do script
+        }
+
+        switch($_FILES['arquivo'] ['type']){
+            case "image/jpeg":
+                $nome_final = $nomearq.".jpeg";
+            break;
+            case "image/jpg":
+                $nome_final = $nomearq.".jpg";
+            break;
+            case "image/png":
+                $nome_final = $nomearq.".png";
+            break;
+            case "image/bmp":
+                $nome_final = $nomearq.".bmp";
+            break;
+            case "application/pdf":
+                $nome_final = $nomearq.".pdf";
+            break;
+            case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+                $nome_final = $nomearq.".docx";
+            break;
+            case "text/plain":
+                $nome_final = $nomearq.".txt";
+            break;
+            default:
+            echo("Formato de arquivo não encontrado");
+            exit;
+        }
+
+        // Pasta onde o arquivo vai ser salvo
+
+        $dir = '../site/images/americo/';
+
+
+        if (move_uploaded_file($_FILES['arquivo'] ['tmp_name'], $dir. '/'. $nome_final)) {
+
+            //Upload efetuado com sucesso, exibe uma mensagem e um link para o arquivo
+            echo "Upload efetuado com sucesso!";
+            echo '<br><a href"'. $dir . '/' . $nome_final . '"> Clique aqui para acessar o arquivo </a>';
+        } else{
+            //Não foi possível fazer o upload, provavelmente a pasta esta incorreta
+            echo "Não foi possível enviar o arquivo, tente novamente";
+        }
     }
-
-    //se o tipo do arquivo não for jpeg ou png, rejeita
-    //if (! ($_FILES['arquivo'] ['type'] == "image/jpeg") || ($_FILES ['arquivo'] ['type'] == "image/png" || ($_FILES ['arquivo'] ['type'] == "image/bmp")))    {
-    //   echo "Por favor, envie arquivos com as seguintes extensões: jpeg, png ou bmp";
-    //    exit; // Para a execução do script
-    //}
-
-    //faz a verificação do tamanho do arquivo
-    //$tam_max = 1024 * 1024 * 5; //5mb
-    //if ($_FILES ['arquivo'] ['size'] > $tam_max) {
-    //   echo "O arquivo enviado é muito grande, envie arquivos de até 5mb.";
-    //    exit; //Para a execuçao do script
-    //}
-
-    //O arquivo passou todas as verificações, hora de tentar movê-lo para a pasta
-
-    // Cria o nome baseado no TIMESTAMP atual e com extensão .jpg
-    // Aqui podemos utilizar alguma informação do úsuario para criar nomes de arquivos personalisados 
-    $nome_final = $nomearq;
-
-    // Pasta onde o arquivo vai ser salvo
-    $dir = 'enviados';
-
-    // Verifica se é possível mover o arquivo para a pasta escolhida
-    if (move_uploaded_file($_FILES['arquivo'] ['tmp_name'], $dir. '/'. $nome_final)) {
-
-        //Upload efetuado com sucesso, exibe uma mensagem e um link para o arquivo
-        echo "Upload efetuado com sucesso!";
-        echo '<br><a href"'. $dir . '/' . $nome_final . '"> Clique aqui para acessar o arquivo </a>';
-    } else{
-        //Não foi possível fazer o upload, provavelmente a pasta esta incorreta
-        echo "Não foi possível enviar o arquivo, tente novamente";
-
-
-    }
-    //move o arquivo da pasta temporaria do servidor para a pasta do site
-    
-    
-
-    echo '<img src = " '.$dir.'/'.$_FILES['arquivo']['name'].'"/>';
-
-}
     else{
         header("Location: principal.php?erro=1");
     }
-    
+    $a1 = $nome_final;
+    echo($_FILES ['arquivo'] ['type']);  
 ?>
 <a href=principal.php>Voltar</a>
+
 	
