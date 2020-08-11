1188df00a0bc8e1afafa47ca38f823271d1dafdf
Mauri <39652613+PCNmauri@users.noreply.github.com>
2020-08-11 14:13:01 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*
+var mudar = document.getElementById('titulo').innerText
+var clicar = document.getElementById('titulo')
+localStorage.setItem('titulo', mudar)
+clicar.addEventListener('click', redirecionar1)
+
+function redirecionar1() {
+        window.location.href = "../template/projects-3.html"
+}
+*/
